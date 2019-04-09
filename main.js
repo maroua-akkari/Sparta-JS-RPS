9abf086a5dff848b2b4a53ea51ae160e76bf8144
Tech-A36 <tech-a36@Tech-A36s-MacBook-Air.local>
2019-04-10 00:23:55 +0100

    added function that compares user/comp input and delacres winner of round

--- main.js
+++ main.js
@@ -2,12 +2,40 @@
 var userChoice = (prompt("Choose 'rock', 'paper' or 'scissors'").toLowerCase());
 
 var compChoice = Math.floor(Math.random() * 3) +1;
-if (compChoice == 0) {
+if (compChoice == 1) {
   compChoice = "rock";
-} else if (compChoice == 1) {
+} else if (compChoice == 2) {
   compChoice = "paper";
 } else {
   compChoice = "scissors";
 }
 
+
 console.log(compChoice);
+
+function compare(user,comp) {
+  if (user == comp) {
+      alert("Tie!")
+  } else if (user == "rock") {
+      if (comp == "paper") {
+          alert("Paper beats rock, computer won this round!");
+      } else {
+        alert("Rock beats scissors, you won this round!");
+      }
+  } else if (user == "paper") {
+      if (comp == "scissors") {
+        alert("Scissors beats paper, computer won this round!");
+      } else {
+        alert("Paper beats rock, you won this round!");
+      }
+  } else if (user == "scissors") {
+    if (comp == "rock") {
+      alert("Rock beats scissors, computer won this round!");
+    } else {
+      alert("Scissors beats paper, you won this round!");
+    }
+  }
+}
+
+var compChose = alert("Computer chose " + compChoice);
+compare(userChoice,compChoice);

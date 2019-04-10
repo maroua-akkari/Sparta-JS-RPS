75060014aed0a1650b3b8f019d0c8534c37e8633
Tech-A36 <tech-a36@Tech-A36s-MacBook-Air.local>
2019-04-10 01:23:56 +0100

    added counter to keep score

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 var userChoice = (prompt("Choose 'rock', 'paper' or 'scissors'").toLowerCase());
 
 var compChoice = Math.floor(Math.random() * 3) +1;
@@ -10,32 +9,40 @@ if (compChoice == 1) {
   compChoice = "scissors";
 }
 
-
-console.log(compChoice);
-
 function compare(user,comp) {
   if (user == comp) {
       alert("Tie!")
   } else if (user == "rock") {
       if (comp == "paper") {
           alert("Paper beats rock, computer won this round!");
+          compScore +=1;
       } else {
         alert("Rock beats scissors, you won this round!");
+        userScore +=1;
       }
   } else if (user == "paper") {
       if (comp == "scissors") {
         alert("Scissors beats paper, computer won this round!");
+        compScore +=1;
       } else {
         alert("Paper beats rock, you won this round!");
+        userScore +=1;
       }
   } else if (user == "scissors") {
     if (comp == "rock") {
       alert("Rock beats scissors, computer won this round!");
+      compScore +=1;
     } else {
       alert("Scissors beats paper, you won this round!");
+      userScore +=1;
     }
   }
 }
 
-var compChose = alert("Computer chose " + compChoice);
-compare(userChoice,compChoice);
+  var compChose = alert("Computer chose " + compChoice);
+  var userScore = 0;
+  var compScore = 0;
+
+  compare(userChoice,compChoice);
+  var scores = alert("Current scores: " + "User: " + userScore + " Computer: " + compScore);
+  
